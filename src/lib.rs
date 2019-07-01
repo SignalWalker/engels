58dbfc01a033c263ab515fd40687ebdbddb7c396
Ash Walker <ash@ashwalker.net>
2019-07-01 11:59:12 -0400

    Made DPDA input generic

--- src/lib.rs
+++ src/lib.rs
@@ -9,21 +9,21 @@ pub enum Action {
 }
 
 /// (Top of stack, Input, Action)
-pub type Transition = (Option<String>, String, Action);
+pub type Transition<C> = (Option<C>, C, Action);
 
 #[derive(Debug)]
-pub struct DPDA {
+pub struct DPDA<C: Clone + PartialEq + Eq> {
     pub accept: HashSet<String>,
-    pub graph: Graph<String, Transition, Directed>,
+    pub graph: Graph<String, Transition<C>, Directed>,
 }
 
-impl Default for DPDA {
+impl<C: Clone + PartialEq + Eq> Default for DPDA<C> {
     fn default() -> Self {
         Self::new()
     }
 }
 
-impl DPDA {
+impl<C: Clone + Clone + PartialEq + Eq> DPDA<C> {
     pub fn new() -> Self {
         Self {
             accept: HashSet::new(),
@@ -38,12 +38,15 @@ impl DPDA {
         self
     }
 
-    pub fn state<'s, I: IntoIterator<Item = (Option<&'s str>, &'s str, Action, &'s str)>>(
+    pub fn state<I: IntoIterator<Item = (Option<C>, C, Action, impl AsRef<str>)>>(
         &mut self,
-        id: &str,
+        id: impl AsRef<str>,
         transitions: I,
     ) -> &mut Self {
-        fn find(graph: &mut Graph<String, Transition, Directed>, id: &str) -> usize {
+        fn find<C: Clone + PartialEq + Eq>(
+            graph: &mut Graph<String, Transition<C>, Directed>,
+            id: &str,
+        ) -> usize {
             match graph.verts.iter().find(|s| match s {
                 Some(s) => s.val == id,
                 None => false,
@@ -53,12 +56,10 @@ impl DPDA {
             }
         }
 
-        let state = find(&mut self.graph, id);
+        let state = find(&mut self.graph, id.as_ref());
 
         for (stack, i, act, next) in transitions {
-            let next = find(&mut self.graph, next);
-            let stack = stack.map(|s| s.to_owned());
-            let i = i.to_string();
+            let next = find(&mut self.graph, next.as_ref());
             self.graph
                 .replace_edge(state, (stack.clone(), i.clone(), act), next, move |e| {
                     e.verts.0 == state && e.weight.0 == stack && e.weight.1 == i
@@ -68,7 +69,7 @@ impl DPDA {
         self
     }
 
-    pub fn runner(&self, acc_empty: bool) -> Runner<'_> {
+    pub fn runner(&self, acc_empty: bool) -> Runner<'_, C> {
         Runner {
             stack: Vec::new(),
             machine: self,
@@ -78,23 +79,23 @@ impl DPDA {
     }
 }
 
-pub struct Runner<'m> {
-    pub machine: &'m DPDA,
-    pub stack: Vec<String>,
+pub struct Runner<'m, C: Clone + PartialEq + Eq> {
+    pub machine: &'m DPDA<C>,
+    pub stack: Vec<C>,
     pub current: usize,
     pub acc_empty: bool,
 }
 
-impl<'m> Runner<'m> {
-    pub fn run<S: AsRef<str>, I: IntoIterator<Item = S>>(
+impl<'m, C: Clone + PartialEq + Eq> Runner<'m, C> {
+    pub fn run<I: IntoIterator<Item = C>>(
         &mut self,
         input: I,
-    ) -> Result<(Vec<Option<String>>, bool), ()> {
+    ) -> Result<(Vec<Option<C>>, bool), ()> {
         Ok((
             {
                 let mut res = Vec::new();
                 for i in input.into_iter() {
-                    res.push(self.next(i.as_ref())?);
+                    res.push(self.next(i)?);
                 }
                 res
             },
@@ -102,7 +103,7 @@ impl<'m> Runner<'m> {
         ))
     }
 
-    pub fn next(&mut self, input: &str) -> Result<Option<String>, ()> {
+    pub fn next(&mut self, input: C) -> Result<Option<C>, ()> {
         use Action::*;
         let vert = self.machine.graph.vert(self.current);
 
@@ -125,13 +126,13 @@ impl<'m> Runner<'m> {
         self.current = edge.verts.1;
         match edge.weight.2 {
             Push => {
-                self.stack.push(input.into());
+                self.stack.push(input);
                 Ok(None)
             }
             Pop => Ok(self.stack.pop()),
             Replace => {
                 let res = self.stack.pop();
-                self.stack.push(input.into());
+                self.stack.push(input);
                 Ok(res)
             }
         }
@@ -153,7 +154,7 @@ mod tests {
     #[test]
     fn machine() {
         use Action::*;
-        let mut machine = DPDA::new();
+        let mut machine: DPDA<&'static str> = DPDA::new();
         machine
             .state(
                 "start",
@@ -165,6 +166,12 @@ mod tests {
             )
             .accept(&["end"]);
         dbg!(&machine);
-        assert!(machine.runner(false).run(&["0", "0", "1", "1"]).unwrap().1);
+        assert!(
+            machine
+                .runner(false)
+                .run(vec!["0", "0", "1", "1"].drain(0..))
+                .unwrap()
+                .1
+        );
     }
 }
